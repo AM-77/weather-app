0a26d30bfefc581b08b9009ae4c05c337bc45f32
AM-77 <amine.griche77@protonmail.ch>
2020-04-15 01:25:13 +0200

    add colors

---
+++ utils/colors.js
@@ -0,0 +1,15 @@
+export const colors = {
+    black: "#000000",
+    white: "#ffffff",
+    dark: "#303030",
+    darkOpacity: "#212121aa",
+    light: "#e1e2e3",
+    lightOpacity: "#e1e2e377"
+}
+
+export const linearColors = {
+    cloudy: [colors.dark, colors.light],
+    clear: ["rgba(45, 161, 253, 1)", "rgba(15, 255, 196, 1)"],
+    cold: ["#3f4c6b", "#606c88"],
+    sunny:  ["#ede574", "#e1f5c4"]
+}
